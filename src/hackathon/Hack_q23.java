bf248b24b6df7c554eda05375fb379d926136e8b
Rupesh Pawar <rupeshpawar@Rupeshs-Air.attlocal.net>
2020-12-21 16:24:31 -0500

    files added

--- src/hackathon/Hack_q23.java
+++ src/hackathon/Hack_q23.java
@@ -3,28 +3,36 @@ package hackathon;
 import java.util.Scanner;
 
 public class Hack_q23 {
-	static void isNumber(String s)
-    {
-        for (int i = 0; i < s.length(); i++)
-            if (Character.isDigit(s.charAt(i)) == true)
-            	 System.out.println("Integer");
- 
-            else if(s.equals("true")|| s.equals("false"))
-            	System.out.println("boolean");
-        
-            else {
-            	System.out.println("String");
-            }
-    }
- 
+	
+ public static boolean is_type(String str) {
+	 for(int i=0;i<str.length();i++) 
+		 if(Character.isDigit(str.charAt(i))==false)
+			 return false;
+		 
+		 return true;
+	 
+ }
+ public static boolean is_bool(String str) {
+	 if(str.equals("true")|| str.equals("false")) 
+		 return true;
+	 
+	 return false;
+	 
+ }
  
     static public void main(String[] args)
     {
-        
-    	Scanner in=new Scanner(System.in);
-		System.out.println("enter input");
-		String str=in.next();
-		isNumber(str);
+        Scanner in =new Scanner(System.in);
+        System.out.println("please enter input");
+        String s=in.next();
+        if(is_bool(s)) {
+        	 System.out.println("it is a boolean");
+        }
+        else if(is_type(s)) {
+        	System.out.println("it is an integer");
+        }else {
+        	System.out.println("it is an string");
+        }
     }
 }
 
---
+++ src/hackathon/Hack_q25.java
@@ -0,0 +1,44 @@
+package hackathon;
+import java.util.Scanner;
+import java.util.Scanner;
+
+public class Hack_q25 {
+	
+	public static void Binary_src(int arr[],int search,int len) {
+		int st=0;
+		int end=len-1;
+		int mid= (st+end)/2;
+		while(st<=end) {
+			if(arr[mid]<search) 
+				st=mid+1;
+			
+			else if(arr[mid]==search) { 
+				System.out.println(search + "item found at location " +(mid + 1));
+				break;
+				}
+			
+			else {
+				end=mid-1;
+			}
+			mid=(st+end)/2;
+		}
+		if ( st > end ) {
+	          System.out.println(search + " is not found.\n");
+	          }
+	}
+
+	public static void main(String[] args) {
+		Scanner in =new Scanner(System.in);
+        System.out.println("please enter input");
+        int n=in.nextInt();
+        int num[]=new int[n];
+        System.out.println("enter array elements");
+        for(int i=0;i<n;i++) {
+        	num[i]=in.nextInt();
+        }
+        System.out.println("please enter search element");
+        int sr=in.nextInt();
+        Binary_src(num,sr,n);
+	}
+
+}
